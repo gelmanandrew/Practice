e6dba5deda59a2cad6377aaf375f4f780502e96c
Andrew Gelman <andrewlancegelman@gmail.com>
2018-08-22 18:16:35 -0700

    HashMap
    Practice with HashMap

--- Calculator.java
+++ Calculator.java
@@ -36,7 +36,7 @@ class Calculator{
       }
   }
   	
-	public static void main (String[] args){
+  public static void main (String[] args){
      	Calculator myCalculator = new Calculator();
       System.out.println(myCalculator.add(5, 7));
       System.out.println(myCalculator.sub(45, 11));
