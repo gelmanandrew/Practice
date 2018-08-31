5f41a16772e0df4a169c34548ef729b1bc2aa073
Andrew Gelman <andrewlancegelman@gmail.com>
2018-08-30 20:54:54 -0700

    more js

--- codecademy/datatypes.js
+++ codecademy/datatypes.js
@@ -21,3 +21,12 @@ primitive data types:
 Another data type:
 
 7) Object: Collections of related data */
+console.log(Math.floor(Math.random()*100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
+
+let changeMe = true;
+console.log(changeMe);
+changeMe = false;
+console.log(changeMe);
+// let can be changed, var stays the same
