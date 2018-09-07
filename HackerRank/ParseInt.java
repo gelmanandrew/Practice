1e2eff22340a29fe9fa0acd5c7882549af1ec593
Andrew Gelman <andrewlancegelman@gmail.com>
2018-09-07 15:50:35 -0700

    parseint

---
+++ HackerRank/ParseInt.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try{
+          int value = Integer.parseInt(S);
+          System.out.println(value);
+        }
+        catch(Exception e)
+        {
+          System.out.println("Bad String");
+        }
+    }
+}
+
+/*
+
+input
+
+3
+
+output
+
+3
+
+
+input
+
+za
+
+output
+
+Bad String
+*/
