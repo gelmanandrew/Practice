e057ab68ecbe3a994dfb587ad67fcef2e70289d7
Andrew Gelman <andrewlancegelman@gmail.com>
2018-09-10 17:43:19 -0700

    binary search tree

---
+++ HackerRank/BST.java
@@ -0,0 +1,41 @@
+class BST{
+    // wrote this
+    public static int getHeight(Node root){
+        //Write your code here
+        if(root == null){
+            return -1;
+        }
+        else{
+            return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+        }
+    }
+    // everything below was given
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Node root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        int height=getHeight(root);
+        System.out.println(height);
+    }
+}
